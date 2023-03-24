3df1f769858b41c897de09cb64d6419c2f7362e5
Extoren <sffjo.aarre@gmail.com>
2023-03-24 11:53:04 +0100

    Update

--- main.js
+++ main.js
@@ -9,4 +9,27 @@ function initialSetup() {
   initialSetup();
 
 
-  
+
+
+
+
+  var elem = document.documentElement;
+        function openFullscreen() {
+          if (elem.requestFullscreen) {
+            elem.requestFullscreen();
+          } else if (elem.mzRequestFullscreen) { /* Safari */
+            elem.webkitRequestFullscreen();
+          } else if (elem.msRequestFullscreen) { /* IE11 */
+            elem.msRequestFullscreen();
+          }
+        }
+        
+        function closeFullscreen() {
+          if (document.exitFullscreen) {
+            document.exitFullscreen();
+          } else if (document.mzExitFullscreen) { /* Safari */
+            document.webkitExitFullscreen();
+          } else if (document.msExitFullscreen) { /* IE11 */
+            document.msExitFullscreen();
+          }
+        }
